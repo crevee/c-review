b0c209da67cba04fb7c775a08a67e4a4823f4400
Ha Sangwook <64821752+crevee@users.noreply.github.com>
2023-06-15 16:12:18 +0900

    Create main.cpp

---
+++ 4. rock_paper_scissors/main.cpp
@@ -0,0 +1,84 @@
+#include <stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+#define GAME_END -1
+#define PLAYER_SCISSORS 0
+#define PLAYER_ROCK 1
+#define PLAYER_PAPER 2
+
+#define PLAYER_WIN 0
+#define PLAYER_LOSE 1
+
+void MyInit(int* count, int* win_count) {
+	*count = 0;
+	*win_count = 0;
+	srand(time(NULL));
+}
+
+int ComPlay() {
+	return rand() % 3;
+}
+
+int PlayerPlay() {
+	int temp;
+
+	printf_s("-1(Exit), 0(Scissors), 1(Rock), 2(Paper)?");
+	scanf_s("%d", &temp);
+
+	return temp;
+}
+
+int PlayerCheck(int com, int player) {
+	if (PLAYER_SCISSORS == player && PLAYER_PAPER == com) {
+		return PLAYER_WIN;
+	}
+	if (PLAYER_ROCK == player && PLAYER_SCISSORS == com) {
+		return PLAYER_WIN;
+	}
+	if (PLAYER_PAPER == player && PLAYER_ROCK == com) {
+		return PLAYER_WIN;
+	}
+	return PLAYER_LOSE;
+}
+
+void ShowRate(int count, int win_count) {
+	float win_rate = 0;
+
+	win_rate = (float)win_count / count;
+
+	if (count == 0) {
+		printf_s("0");
+		return;
+	}
+
+	printf_s("Win Rate :  %.2f \n", win_rate);
+}
+
+int main() {
+
+	int player, com, re, count = 0, win_count = 0;
+	float win_rate;
+
+	MyInit(&count, &win_count);
+
+	while (1) {
+		com = ComPlay();
+		player = PlayerPlay();
+
+		if (GAME_END == player) {
+			break;
+		}
+		count++;
+		re = PlayerCheck(com, player);
+
+		if (PLAYER_WIN == re) {
+			win_count++;
+			printf_s("You Win! c:%d y:%d \n", com, player);
+		}
+		else {
+			printf_s("You Lose! c:%d y:%d \n", com, player);
+		}
+	}
+	ShowRate(count, win_count);
+}
